5b49a29a7d1a685d4219cfaebef717ecef1cfd80
muskanYadav <166051734+Mvskywert345@users.noreply.github.com>
2025-02-28 14:49:51 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -48,7 +48,7 @@ function Home2() {
               </i>
               &nbsp; like
               <i>
-                <b className="pink"> React.js and Node.js</b>
+                <b className="pink"> React.js and Three.js</b>
               </i>
             </p>
           </Col>
